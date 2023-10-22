44d14ce0d35e963c90956367c26ebfe0615a9408
Augusto Pastrana <aspastrana990@gmail.com>
2023-10-22 20:53:59 -0300

    Change BASE_URL .env variable

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -3,6 +3,6 @@ import { Controller, Get, Redirect } from '@nestjs/common';
 @Controller({ path: '' })
 export class AppController {
   @Get()
-  @Redirect(`${process.env.BASE_URL}/info`, 301)
+  @Redirect(`${process.env.RAILWAY_PUBLIC_DOMAIN}/info`, 301)
   appInfo() {}
 }
