8f2152ce3542a24366c2a6bccf191091be0bc77f
Augusto Pastrana <aspastrana990@gmail.com>
2023-10-23 02:06:34 -0300

    Add Notion url

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -8,7 +8,8 @@ export class InfoService {
       version: '1.0.0',
       githubRepo: 'https://github.com/blanck1945/nest-movie-app',
       swagger: 'https://nest-movie-app-production.up.railway.app/api',
-      notion: '',
+      notion:
+        'https://keen-atom-d66.notion.site/CONEXA-challenge-da8eb3247c9d47818c0831346f8b2cea',
     };
   }
 }
